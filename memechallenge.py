93a6d6f9468824bb0165bbb606d54d0e7366c202
Francisco Molinero <francisco_m253@yahoo.com>
2022-01-04 18:15:04 +0000

    learning about dictionaries

---
+++ memechallenge.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+icecream= ["indentation", "spaces"] 
+
+tlgstudents= ["Aaron","Alex","Alonzo","Brandon","Chris","Francisco","James","Jonathan","Lillian","Manuel","Patrick","Robert","Ryan","Troy","Wes","Henry","Yalined"]
+
+choice= int(input("Pick a student number!"))
+student_name= tlgstudents[choice]
