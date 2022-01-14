a1e938f9faf038a0094e6e2b1ca093577122ed56
Francisco Molinero <francisco_m253@yahoo.com>
2022-01-14 05:05:30 +0000

    Project 2

--- project2/minproj2.py
+++ project2/minproj2.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 
-#using Lilian's code and modfying for use in different file
 
-#importing pandas
-import pandas as pd
-
-#creating header list
-header_list= ["Issue", "Number", "Arc", "Distributor", "ID", "Contributor"]
+#opening .txt file in read mode
+file = open("brett_comics.txt", "r")
 
 
-df = pd.read_csv('brett_comics.txt', index_col="Contributor")
-i = int(input("Please enter as shown either Andy Kubert or Fabian Nicieza: "))
-comics_df= df.loc[i]
+#reading first 63 characters in file and printing to CLI
+print(file.read(63))
 
-comics_df.drop(comics_df.columns.difference(header_list), 1, inplace=True)
-print(comics_df.head(15))         
+
+#closing file
+file.close()
